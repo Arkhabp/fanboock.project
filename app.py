8405b8a837860ea8c7ccb4ffc9369be26b2dd949
LAPTOP-I43HEEAB\windows-10 <arkhabagdana1@gmail.com>
2023-04-10 15:16:07 +0700

    commit app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://khab:khab@cluster0.gcehkth.mongodb.net/?retryWrites=true&w=majority')
+db = client.fanboock
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/homework", methods = ["POST"])
+def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name' : name_receive,
+        'comment' : comment_receive
+    }
+    db.fanmessage.insert_one(doc)
+    return jsonify({'msg' : 'POST request!'})
+
+@app.route("/homework", methods = ["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id' : False}))
+    return jsonify({'messages' : message_list})
